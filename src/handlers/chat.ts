5986ee2d5668e3a993b903238992fe988d3ae222
midnqp <midnightquantumprogrammer@gmail.com>
2023-11-03 11:14:13 +0600

    chore: removed code for mic and minorly organized

--- src/handlers/chat.ts
+++ src/handlers/chat.ts
@@ -1,75 +1,54 @@
 import services from '@src/services/index.js'
 
-export default async function () {
-    services.linenoise.getPrompt = async () => {
-        let result
+class ChatnetChatHandler {
+    constructor() { }
+    
+    action() {
+        services.linenoise.getPrompt = async () => {
+            let result
 
-        const username = await services.config.get('username')
-        const name = username || '<username-not-set>'
-        result = name + ': '
+            const username = await services.config.get('username')
+            const name = username || '<username-not-set>'
+            result = name + ': '
 
-        if (services.mic.isTurnedOn)
-        result = '🎙️ ' + result
+            return result
+        }
+        services.linenoise.start(this.handleMsg)
 
-        return result
-    }
-    services.linenoise.start(handleMsg)
-
-    services.receive.start()
-}
-
-async function handleMsg(msg: string) {
-    let doOneMore = true
-
-    const msgSplit = msg.split(' ')
-    switch (msgSplit[0]) {
-        case '/exit':
-            handleExit()
-            doOneMore = false
-            break
-        case '/name':
-            await handleSetName(msgSplit[1]) // await this one.
-            break
-        case '/mic':
-            await handleMicToggle() // await if you want to show the 🎙️ icon
-        default:
-            // regular text messages
-            break
+        services.receive.start()
     }
 
-    return doOneMore
-}
+    async handleMsg(msg: string) {
+        let doOneMore = true
 
-async function handleExit() {
-    services.linenoise.close()
-    services.stdoutee.close()
-    services.receive.close()
-    services.api.close()
-}
+        const msgSplit = msg.split(' ')
+        switch (msgSplit[0]) {
+            case '/exit':
+                this.exit()
+                doOneMore = false
+                break
+            case '/name':
+                await this.handleSetName(msgSplit[1]) // await this one.
+                break
+            default:
+                // regular text messages
+                break
+        }
 
-async function handleSetName(name: string) {
-    // todo refactor
-    await services.api.setOrUpdateName(name)
-}
-
-async function handleMicToggle() {
-    if (!services.mic.isTurnedOn) {
-        await services.mic.start()
-
-        services.mic.setTimeoutFn(function () {
-            services.mic.stop()
-            const audio = services.mic.getAudio()
-            services.api.sendVoicemessage(audio)
-        }, 5000)
-    }
-    else {
-        services.mic.stop()
-        const audio = services.mic.getAudio()
-        services.api.sendVoicemessage(audio)
-        services.mic.close()
+        return doOneMore
     }
 
-    services.mic.isTurnedOn = !services.mic.isTurnedOn
+    async handleSetName(name: string) {
+        // todo refactor
+        await services.api.setOrUpdateName(name)
+    }
+
+    exit() {
+        services.linenoise.close()
+        services.stdoutee.close()
+        services.receive.close()
+        services.api.close()
+    }
 }
 
-function recursiveVoiceRecord() { }
+export default new ChatnetChatHandler()
