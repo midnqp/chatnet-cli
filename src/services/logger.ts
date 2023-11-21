55112b850958b5bdfc35dd944c1595664ed7d39d
midnqp <midnightquantumprogrammer@gmail.com>
2023-11-21 14:22:32 +0600

    fix: used pino.destination instead

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -6,17 +6,22 @@ import os from 'node:os'
 class ChatnetLogger {
     constructor() {
         const logFile = path.join(os.homedir(), '.chatnet.log')
-        const fileStream = fs.createWriteStream(logFile, 'utf8')
-        this.logger = pino({write:fileStream.write})
-        this.error = this.logger.error
-        this.info = this.logger.info
+        this.logger = pino(pino.destination(logFile))
+        //this.error = this.logger.error
+        //this.info = this.logger.info
     }
 
     private logger
 
-    public error
+    public error(...msg:any) {
+        msg = msg.join(' ')
+        return this.logger.error(msg)
+    }
 
-    public info
+    public info(...msg:any){
+        msg=msg.join(' ')
+        return this.logger.info(msg)
+    }
 }
 
 export default new ChatnetLogger()
