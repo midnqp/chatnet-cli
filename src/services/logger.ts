a08de352bc52534166c9372be2745b5191fa9220
midnqp <midnightquantumprogrammer@gmail.com>
2023-11-22 21:26:13 +0600

    feat(logger): added support for any data type instead of just strings

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,10 +1,14 @@
 import pino from 'pino'
+import pinoPretty from 'pino-pretty'
 import fs from 'node:fs'
 import path from 'node:path'
 import os from 'node:os'
+import { inspect } from 'node:util'
 
 class ChatnetLogger {
-    private logger = pino(pino.destination(path.join(os.homedir(), '.chatnet.log')))
+    //private logger = pino(pino.destination(path.join(os.homedir(), '.chatnet.log')))
+    private loggerDestination = pino.destination(path.join(os.homedir(), '.chatnet.log'))
+    private logger = pino(pinoPretty({destination: this.loggerDestination}))
 
     private nullLogger = pino({enabled:false})
 
@@ -16,8 +20,13 @@ class ChatnetLogger {
 
     public info(...msg:any){
         if (!process.env.CHATNET_DEBUG) return
-        msg=msg.join(' ')
-        this.logger.info(msg)
+        
+        let result = ''
+        for (let m of msg) {
+            result += inspect(m)+' '
+        }
+
+        this.logger.info(result )
     }
 }
 
